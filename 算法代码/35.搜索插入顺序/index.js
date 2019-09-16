acb6f76c54d651892e1fdf10d24c2a0f3bcc39b3
webbj97 <webbj97@163.com>
2019-09-16 14:49:45 +0800

    修改

--- 算法代码/35.搜索插入顺序/index.js
+++ 算法代码/35.搜索插入顺序/index.js
@@ -16,20 +16,37 @@
 //     return left+1
 // };
 
+// var searchInsert = function(nums, target) {
+//     var left = 0;
+//     var right = nums.length-1
+//     if(target > nums[right]) return right+1
+//     while(left < right){
+//         let mid = Math.floor((left + right)/2)
+//         if(nums[mid] == target){
+//             return mid
+//         }else if(nums[mid] < target){
+//             left = mid + 1
+//         }else{
+//             right = mid
+//         }
+//     }
+//     return left
+// };
+
 var searchInsert = function(nums, target) {
-    var left = 0;
-    var right = nums.target-1
-    while(left < right){
-        let mid = Math.floor((left + right)/2)
-        if(nums[mid] == target){
-            return mid
-        }else if(nums[mid] < target){
-            left = mid + 1
-        }else{
-            right = mid - 1
+    var res = 0,len = nums.length
+    if(nums[0] > target){
+        return 0
+    }
+    if(nums[len-1] < target){
+        return len
+    }
+    for(var i = 0; i < len; i++){
+        if(nums[i] >= target){
+            return i
         }
     }
-    return left
 };
-var res = searchInsert([1,3,5,6], 0)
+var res = searchInsert([1,3,5,6], 7)
+
 console.log(res)
