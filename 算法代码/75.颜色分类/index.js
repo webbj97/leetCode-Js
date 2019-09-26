7b24e3e2aed60090cd841fa03d47e93772160575
webbj97 <webbj97@163.com>
2019-09-26 16:42:01 +0800

    修改readme

--- 算法代码/75.颜色分类/index.js
+++ 算法代码/75.颜色分类/index.js
@@ -3,35 +3,31 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    var res = [],len = nums.length
-    var idx = 0
-    var temp1 = 0
-    var temp2 = 0
-    var left  = 0,right = nums.length-1
-    // nums.forEach(e => {
-    //     if(e == 0){
-    //         res.unshift(e)
-    //         idx++
-    //     }else if(e == 2){
-    //         res.push(e)
-    //     }
-    // });
-    for(let i = 0; i < len; i++){
-        if(nums[i] == 0){
-            temp1 = nums[i]
-            nums[left] = temp1
-            nums[i] = nums[left]
-            left++
-        }else if(nums[i] == 2){
-            temp2 = nums[i]
-            nums[right] = temp2
-            nums[i] = nums[right]
-            right--
-        }
+    var left = 0,
+        idx = 0; //左边界
+    var right = nums.length - 1; //右边界
+
+    var temp; //中间值
+    while (idx <= right) {
+        if (nums[idx] == 0) {
+            // 交换第 left个和第idx个元素
+            // i++，j++
+            temp = nums[left];
+            nums[left++] = nums[idx];
+            nums[idx++] = temp;
+        } else if (nums[idx] == 2) {
+            // 交换第k个和第idx个元素
+            // right--
+            temp = nums[idx];
+            nums[idx] = nums[right];
+            nums[right--] = temp;
+        } else idx++;
     }
     return nums
 };
 
-var data = [2,0,2,1,1,0]
+
+
+var data = [2, 0, 2, 1, 1, 0]
 var res = sortColors(data)
-console.log(res,'res')
+console.log(res, 'res')
