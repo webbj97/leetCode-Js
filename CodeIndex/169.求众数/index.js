6083edb5a0f343c5c19b2c5d7b1d955405159909
yuguang <yuguang@gaoding.com>
2019-10-13 16:36:03 +0800

    修改REAMDE

--- CodeIndex/169.求众数/index.js
+++ CodeIndex/169.求众数/index.js
@@ -1,20 +1,27 @@
+// var majorityElement = function(nums) {
+//     var obj = {}
+//     for(let i = 0; i < nums.length; i++){
+//         if(obj[nums[i]]){
+//             obj[nums[i]]++
+//         }else{
+//             obj[nums[i]] = 1
+//         }
+//     }
+//     var max = 0, res = nums
+//     for(var key in obj){
+//         if(obj[key] > max) {
+//             max = obj[key]
+//             res = key
+//         }
+//     }
+//     return res
+// };
+
 var majorityElement = function(nums) {
-    var obj = {}
-    for(let i = 0; i < nums.length; i++){
-        if(obj[nums[i]]){
-            obj[nums[i]]++
-        }else{
-            obj[nums[i]] = 1
-        }
-    }
-    var max = 0, res = nums
-    for(var key in obj){
-        if(obj[key] > max) {
-            max = obj[key]
-            res = key
-        }
-    }
-    return res
+    nums.sort((a, b)=>{
+        return a-b
+    })
+    return nums[Math.floor(nums.length/2)]
 };
 
 var res = majorityElement([2,2,1,1,1,2,2])
--- README.md
+++ README.md
@@ -33,7 +33,7 @@ E-mail：webbj97@163.com
 |[88.合并两个有序数组](https://github.com/webbj97/leetCode-JavaScript-bj/blob/master/code/4.%E5%90%88%E5%B9%B6%E4%B8%A4%E4%B8%AA%E6%9C%89%E5%BA%8F%E6%95%B0%E7%BB%84.md)|暂为二种|42.9%|简单|数组|
 |[125.验证回文串](https://github.com/webbj97/leetCode-JavaScript-bj/blob/master/code/8.%E5%9B%9E%E6%96%87%E5%AD%97%E7%AC%A6%E4%B8%B2.md)|暂为一种|52.2%|简单|字符串|
 |[141.环形链表]()|二种|43.7%|简单|链表|
-|[169.求众数](https://blog.csdn.net/jbj6568839z/article/details/103072787)|一种|43.7%|简单|数组|
+|[169.求众数](https://blog.csdn.net/jbj6568839z/article/details/103072787)|二种|43.7%|简单|数组|
 |[189.旋转数组](https://blog.csdn.net/jbj6568839z/article/details/103118847)|四种|33.7%|中等|数组|
 |[202.快乐数](https://github.com/webbj97/leetCode-JavaScript-bj/blob/master/code/5.%E5%BF%AB%E4%B9%90%E6%95%B0.md)|暂为一种|52.2%|简单|数组|
 |[206.反转链表](https://blog.csdn.net/jbj6568839z/article/details/102662509)|一种|52.2%|简单|链表|
