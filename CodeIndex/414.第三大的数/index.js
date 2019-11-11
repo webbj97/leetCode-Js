1a05bff591555e13e28fb352b332eab0fb027c81
yuguang <yuguang@gaoding.com>
2019-11-11 11:55:04 +0800

    调整README

---
+++ CodeIndex/414.第三大的数/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    const len = nums.length
+    if(len < 3){
+        return Math.max(...nums)
+    }
+    var obj = {}
+    for (let i = 0; i < len; i++) {
+    }
+};
