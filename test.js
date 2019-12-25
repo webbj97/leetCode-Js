2b208acc9a1c2f9352c5f123273440cbb7792f88
yuguang <yuguang@gaoding.com>
2019-12-25 17:01:23 +0800

    leetCode

--- test.js
+++ test.js
@@ -1,19 +1,5 @@
-function deep(obj) {
-    var arr = []
-    if(typeof obj !== 'Object'){
-        arr = obj
-    }else{
-        for(var key in obj){
-            deep(obj[key])
-        }
-    }
-    return arr
+var a = new Boolean('')
+console.log('a:', a);
+if(a){
+    console.log('1111:');
 }
-
-var obj = {
-    a: '11',
-    name: [1,2,3],
-    test: '666'
-}
-var res = deep(obj)
-console.log('res:', res);
